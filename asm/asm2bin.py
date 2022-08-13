c13f11b9b7854361dd9211b688b6e03bd68b4c1b
nguyenducquang1212 <quangnguyenduc1212@gmail.com>
2022-08-14 00:37:56 +0700

    update instr

--- asm/asm2bin.py
+++ asm/asm2bin.py
@@ -12,16 +12,14 @@ cnv.convert("simple.s")
 f = open(pathRiscV+"simple/txt/simple.txt", "r")
 instr = ""
 for x in f:
-  s3 = x[24:31]
-  s2 = x[16:23]
-  s1 = x[8:15]
-  s0 = x[0:7]
-  instr += s0 + "\n"
-  instr += s1 + "\n"
-  instr += s2 + "\n"
+  s3 = x[24:32]
+  s2 = x[16:24]
+  s1 = x[8:16]
+  s0 = x[0:8]
   instr += s3 + "\n"
-
-print(instr)
+  instr += s2 + "\n"
+  instr += s1 + "\n"
+  instr += s0 + "\n"
 
 f = open("C:/Users/Dell Latitude/OneDrive - Hanoi University of Science and Technology/Documents/Tai Lieu DH/Kien truc may tinh/risc_v/sim/tb/inst.txt", "w")
 f.write(instr)

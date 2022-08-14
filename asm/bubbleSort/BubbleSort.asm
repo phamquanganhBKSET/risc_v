cadd54c5bef1fc011678c1e5f9e450ac5a1d5600
phamquanganh <phamquanganh705@gmail.com>
2022-08-14 16:56:57 +0700

    update saam

--- asm/bubbleSort/BubbleSort.asm
+++ asm/bubbleSort/BubbleSort.asm
@@ -6,6 +6,7 @@ loop1:
 beq x6 x7 exit
 sub x8 x7 x6
 addi x9 x0 0
+
 loop2:
 beq x9 x8 next
 add x10 x9 x9
@@ -17,11 +18,13 @@ lw x13 0(x12)
 blt x13 x11 swap
 addi x9 x9 1
 jal x0 loop2
+
 swap:
 sw x11 0(x12)
 sw x13 0(x10)
 addi x9 x9 1
 jal x0 loop2
+
 next:
 addi x6 x6 1
 jal x0 loop1
